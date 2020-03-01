1afbef1db09c7d5ad77f70b79575c05419704b54
Brittany Russell <sincerelybr@gmail.com>
2020-03-01 02:26:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
+# Dog
+#   is defined within lib/dog.rb
+#   with names
+#     #name=
+#       writes the name of the dog to an instance variable @name
+#     #name
+#       reads the name of the dog from an instance variable @name
+#   with breeds
+#     #breed=
+#       writes the breed of the dog to an instance variable @breed
+#     #breed
+#       reads the breed of the dog from an instance variable @breed
+
 class Dog
     def name=(name)
     @name = name
